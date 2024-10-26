3956e99f5382c246da92de6b8b55f8876fe4fdab
zulkifl7 <zulkiflajmal@gmail.com>
2024-10-26 18:59:27 +0530

    Added : Basic game logic to determine the winner

--- src/components/GameContainer/GameBoard/GameBoard.jsx
+++ src/components/GameContainer/GameBoard/GameBoard.jsx
@@ -2,55 +2,117 @@ import "./GameBoard.css";
 import Box from "./Box/Box";
 import imageX from "../../../assets/x.svg";
 import imageO from "../../../assets/o.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 // Initial empty game board state
 const gameBoard = [
-	[null, null, null],
-	[null, null, null],
-	[null, null, null],
+	[<img src="" />, <img src="" />, <img src="" />],
+	[<img src="" />, <img src="" />, <img src="" />],
+	[<img src="" />, <img src="" />, <img src="" />],
 ];
 
-// const wins = [[(0,0),(0,1),(0,2)]
+const winningCombinations = [
+	// Horizontal Wins
+	[
+		[0, 0],
+		[0, 1],
+		[0, 2],
+	], // Top row
+	[
+		[1, 0],
+		[1, 1],
+		[1, 2],
+	], // Middle row
+	[
+		[2, 0],
+		[2, 1],
+		[2, 2],
+	], // Bottom row
+
+	// Vertical Wins
+	[
+		[0, 0],
+		[1, 0],
+		[2, 0],
+	], // Left column
+	[
+		[0, 1],
+		[1, 1],
+		[2, 1],
+	], // Middle column
+	[
+		[0, 2],
+		[1, 2],
+		[2, 2],
+	], // Right column
+
+	// Diagonal Wins
+	[
+		[0, 0],
+		[1, 1],
+		[2, 2],
+	], // Top-left to bottom-right
+	[
+		[0, 2],
+		[1, 1],
+		[2, 0],
+	], // Top-right to bottom-left
+];
+
+const checkWinner = (board) => {
+	for (const combination of winningCombinations) {
+		const [a, b, c] = combination;
+		if (
+			board[a[0]][a[1]].props.src &&
+			board[a[0]][a[1]].props.src === board[b[0]][b[1]].props.src &&
+			board[a[0]][a[1]].props.src === board[c[0]][c[1]].props.src
+		) {
+			return board[a[0]][a[1]]; // Return the winner (X or O)
+		}
+	}
+	return null; // No winner
+};
+
 let num = 0; // Variable to keep track of the turn (odd or even)
 
 function GameBoard({ onSelectSquare }) {
-	// State to hold the current state of the game board
 	const [newGameBoard, setGameBoard] = useState(gameBoard);
+	const [winner, setWinner] = useState(null);
 
-	// Function to handle button clicks, updating the board
-	function buttonClickedHandle(row, col) {
-		// [<img src={imageX} />,<img src={imageO} />].map((elm) => {
-
-		// })
-		if (newGameBoard[row][col] === null) {
-			if (num % 2 == 0) {
-				// Player X's turn
-				setGameBoard((pervGameBoard) => {
-					// Create a deep copy of the current game board
-					const updatedBoard = [
-						...pervGameBoard.map((innerArray) => [...innerArray]),
-					];
-					// Place X's image at the clicked position
-					updatedBoard[row][col] = <img src={imageX} />;
-					return updatedBoard;
-				});
-			} else {
-				// Player O's turn
-				setGameBoard((pervGameBoard) => {
-					// Create a deep copy of the current game board
-					const updatedBoard = [
-						...pervGameBoard.map((innerArray) => [...innerArray]),
-					];
-					// Place O's image at the clicked position
-					updatedBoard[row][col] = <img src={imageO} />;
-					return updatedBoard;
-				});
-			}
-			onSelectSquare(); // Call the function to change the active player
-			num += 1; // Increment the counter to switch turns
+	// Check for a winner whenever the game board changes
+	useEffect(() => {
+		const winner = checkWinner(newGameBoard);
+		if (winner) {
+			console.log(`${winner.props.src === imageX ? "X" : "O"} Won`);
+			setWinner(winner);
 		}
-		console.log(newGameBoard); // Log the current state of the board
+	}, [newGameBoard]);
+
+	function buttonClickedHandle(row, col) {
+		if (winner) return; // If there's a winner, do not allow further clicks
+
+		if (num % 2 === 0) {
+			// Player X's turn
+			setGameBoard((prevGameBoard) => {
+				const updatedBoard = prevGameBoard.map((innerArray) => [
+					...innerArray,
+				]);
+				updatedBoard[row][col] = <img src={imageX} />;
+				return updatedBoard;
+			});
+		} else {
+			// Player O's turn
+			setGameBoard((prevGameBoard) => {
+				const updatedBoard = prevGameBoard.map((innerArray) => [
+					...innerArray,
+				]);
+				updatedBoard[row][col] = <img src={imageO} />;
+				return updatedBoard;
+			});
+		}
+
+		onSelectSquare(); // Call the function to change the active player
+		num += 1; // Increment the counter to switch turns
 	}
 
 	return (
@@ -66,19 +128,12 @@ function GameBoard({ onSelectSquare }) {
 									buttonClickedHandle(index, elemIndex)
 								}
 								disabled={
-									newGameBoard[index][elemIndex] === null
-										? false
-										: true
+									newGameBoard[index][elemIndex].props.src
+										.length > 0 || winner !== null
 								}
 							>
 								{element}
 							</button>
-							{/* Optional: Use the Box component if needed */}
-							{/* <Box
-                boxNumber={[index, elemIndex]}
-                buttonClicked={buttonClickedHandle}
-                value={element}
-              /> */}
 						</li>
 					))}
 				</ol>
